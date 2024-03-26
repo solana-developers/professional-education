56bd06d5d88d40d10d2747ecd279f64d5c63d56b
Mike MacCana <mike.maccana@gmail.com>
2024-03-26 08:24:40 -0400

    Add create-token-account

---
+++ create-token-account.ts
@@ -0,0 +1,39 @@
+import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import "dotenv/config";
+import {
+  getExplorerLink,
+  getKeypairFromEnvironment,
+} from "@solana-developers/helpers";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const sender = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `🔑 Loaded our keypair securely, using an env file! Our public key is: ${sender.publicKey.toBase58()}`
+);
+
+// Subtitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey(
+  "HYeUCAqdsQBkqQNHRoBPov42QySDhwM7zAqiorToosbz"
+);
+
+// Subtitute in a recipient from addresses.ts
+const recipient = new PublicKey("Hxsgo2tPiu6967VUaEquk232riDKkaqK89wBvdSCgjH7");
+
+const tokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  sender,
+  tokenMintAccount,
+  recipient
+);
+
+console.log(`Token Account: ${tokenAccount.address.toBase58()}`);
+
+const link = getExplorerLink(
+  "address",
+  tokenAccount.address.toBase58(),
+  "devnet"
+);
+
+console.log(`✅ Created token Account: ${link}`);
