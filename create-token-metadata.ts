7d20fb8937d5694709f6dd9618990c1cad8dd56f
Mike MacCana <mike.maccana@gmail.com>
2024-03-26 22:39:55 -0400

    add create-token-metadata-account

---
+++ create-token-metadata.ts
@@ -0,0 +1,97 @@
+// This uses "@metaplex-foundation/mpl-token-metadata@2" to create tokens
+import "dotenv/config";
+import {
+  getKeypairFromEnvironment,
+  getExplorerLink,
+} from "@solana-developers/helpers";
+import {
+  Connection,
+  clusterApiUrl,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(
+  `🔑 We've loaded our keypair securely, using an env file! Our public key is: ${user.publicKey.toBase58()}`
+);
+
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
+// Subtitute in your token mint account from create-token-mint.ts
+const tokenMintAccount = new PublicKey(
+  "HNqDcFz6UNvfTZYFvZCDiV4FsZLk9umXZGaqNV6HA6TW"
+);
+
+const metadataData = {
+  name: "Solana Training Token",
+  symbol: "TRAINING",
+  // Arweave / IPFS / Pinata etc link using metaplex standard for off-chain data
+  uri: "https://arweave.net/1234",
+  sellerFeeBasisPoints: 0,
+  creators: null,
+  collection: null,
+  uses: null,
+};
+
+const metadataPDAAndBump = PublicKey.findProgramAddressSync(
+  [
+    Buffer.from("metadata"),
+    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+    tokenMintAccount.toBuffer(),
+  ],
+  TOKEN_METADATA_PROGRAM_ID
+);
+
+const metadataPDA = metadataPDAAndBump[0];
+
+const transaction = new Transaction();
+
+const createMetadataAccountInstruction =
+  createCreateMetadataAccountV3Instruction(
+    {
+      metadata: metadataPDA,
+      mint: tokenMintAccount,
+      mintAuthority: user.publicKey,
+      payer: user.publicKey,
+      updateAuthority: user.publicKey,
+    },
+    {
+      createMetadataAccountArgsV3: {
+        collectionDetails: null,
+        data: metadataData,
+        isMutable: true,
+      },
+    }
+  );
+
+transaction.add(createMetadataAccountInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [user]
+);
+
+const transactionLink = getExplorerLink(
+  "transaction",
+  transactionSignature,
+  "devnet"
+);
+
+console.log(`✅ Transaction confirmed, explorer link is: ${transactionLink}!`);
+
+const tokenMintLink = getExplorerLink(
+  "address",
+  tokenMintAccount.toString(),
+  "devnet"
+);
+
+console.log(`✅ Look at the token mint again: ${tokenMintLink}!`);
