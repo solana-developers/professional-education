8e35092acad03d6f9ffd20b2eeef3661433f77e3
Mike MacCana <mike.maccana@gmail.com>
2024-04-01 09:44:18 -0400

    Move labs into lab dir

--- labs/create-token-metadata.ts
+++ labs/create-token-metadata.ts
@@ -25,10 +25,8 @@ const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
   "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 );
 
-// Subtitute in your token mint account from create-token-mint.ts
-const tokenMintAccount = new PublicKey(
-  "HNqDcFz6UNvfTZYFvZCDiV4FsZLk9umXZGaqNV6HA6TW"
-);
+// Subtitute in your token mint account
+const tokenMintAccount = new PublicKey("YOU_TOKEN_MINT_ADDRESS_HERE");
 
 const metadataData = {
   name: "Solana Training Token",
