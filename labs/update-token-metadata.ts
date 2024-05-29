9f015470e47b992ecdf6ebf9edae4e938969159d
Mike MacCana <mike.maccana@gmail.com>
2024-05-29 16:56:06 -0400

    Add script to update token metadata

---
+++ labs/update-token-metadata.ts
@@ -0,0 +1,98 @@
+// This uses "@metaplex-foundation/mpl-token-metadata@2" to create tokens
+import "dotenv/config";
+import {
+  getKeypairFromEnvironment,
+  getExplorerLink,
+} from "@solana-developers/helpers";
+import {
+  Connection,
+  clusterApiUrl,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import {
+  DataV2,
+  createUpdateMetadataAccountV2Instruction,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+const user = getKeypairFromEnvironment("SECRET_KEY");
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(
+  `🔑 We've loaded our keypair securely, using an env file! Our public key is: ${user.publicKey.toBase58()}`
+);
+
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+
+// Subtitute in your token mint account
+const tokenMintAccount = new PublicKey("YOUR_TOKEN_MINT_ADDRESS_HERE");
+
+const metadataData: DataV2 = {
+  name: "Solana Training Token",
+  symbol: "TRAINING",
+  // An off-chain link to more information about the token using Metaplex standard for off-chain data
+  // We are using a GitHub link here, but in production this content would be hosted on an immutable storage like
+  // Arweave / IPFS / Pinata etc
+  uri: "https://raw.githubusercontent.com/solana-developers/professional-education/main/labs/sample-token-metadata.json",
+  sellerFeeBasisPoints: 0,
+  creators: null,
+  collection: null,
+  uses: null,
+};
+
+const metadataPDAAndBump = PublicKey.findProgramAddressSync(
+  [
+    Buffer.from("metadata"),
+    TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+    tokenMintAccount.toBuffer(),
+  ],
+  TOKEN_METADATA_PROGRAM_ID
+);
+
+const metadataPDA = metadataPDAAndBump[0];
+
+const transaction = new Transaction();
+
+const updateMetadataAccountInstruction =
+  createUpdateMetadataAccountV2Instruction(
+    {
+      metadata: metadataPDA,
+      updateAuthority: user.publicKey,
+    },
+    {
+      updateMetadataAccountArgsV2: {
+        data: metadataData,
+        updateAuthority: user.publicKey,
+        primarySaleHappened: true,
+        isMutable: true,
+      },
+    }
+  );
+
+transaction.add(updateMetadataAccountInstruction);
+
+const transactionSignature = await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [user]
+);
+
+const transactionLink = getExplorerLink(
+  "transaction",
+  transactionSignature,
+  "devnet"
+);
+
+console.log(`✅ Transaction confirmed, explorer link is: ${transactionLink}!`);
+
+const tokenMintLink = getExplorerLink(
+  "address",
+  tokenMintAccount.toString(),
+  "devnet"
+);
+
+console.log(`✅ Look at the token mint again: ${tokenMintLink}!`);
