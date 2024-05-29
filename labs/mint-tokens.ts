339679ef2e72657f7481776936fd52d61f7c1f40
Mike MacCana <mike.maccana@gmail.com>
2024-05-29 16:55:46 -0400

    Mint tokens to ourselves, we will transfer them to other people later

--- labs/mint-tokens.ts
+++ labs/mint-tokens.ts
@@ -1,11 +1,13 @@
-import { mintTo } from "@solana/spl-token";
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 import "dotenv/config";
 import {
   getExplorerLink,
   getKeypairFromEnvironment,
 } from "@solana-developers/helpers";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-const connection = new Connection(clusterApiUrl("devnet"));
+
+// mintTo() doesn't default to a commitment level (unlike, say, sendAndConfirmTransaction() ), so we need to specify it
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
 // Our token has two decimal places
 const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
@@ -15,16 +17,19 @@ const user = getKeypairFromEnvironment("SECRET_KEY");
 // Subtitute in your token mint account from create-token-mint.ts
 const tokenMintAccount = new PublicKey("YOUR_TOKEN_MINT_HERE");
 
-// Substitute in your classmate's wallet address here
-const recipientAssociatedTokenAccount = new PublicKey(
-  "RECIPIENT_TOKEN_ACCOUNT_HERE"
+// Let's mint the tokens to ourselves for now!
+const recipientAssociatedTokenAccount = await getOrCreateAssociatedTokenAccount(
+  connection,
+  user,
+  tokenMintAccount,
+  user.publicKey
 );
 
 const transactionSignature = await mintTo(
   connection,
   user,
   tokenMintAccount,
-  recipientAssociatedTokenAccount,
+  recipientAssociatedTokenAccount.address,
   user,
   10 * MINOR_UNITS_PER_MAJOR_UNITS
 );
