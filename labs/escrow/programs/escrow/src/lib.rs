d4727302a7a763eae20632ac74a912aa36a251ac
Mike MacCana <mike.maccana@gmail.com>
2024-05-30 23:52:50 -0400

    Use handlers rather than 'instructions'

--- labs/escrow/programs/escrow/src/lib.rs
+++ labs/escrow/programs/escrow/src/lib.rs
@@ -1,12 +1,12 @@
 pub mod constants;
 pub mod error;
-pub mod instructions;
+pub mod handlers;
 pub mod state;
 
 use anchor_lang::prelude::*;
 
 pub use constants::*;
-pub use instructions::*;
+pub use handlers::*;
 pub use state::*;
 
 declare_id!("qbuMdeYxYJXBjU6C6qFKjZKjXmrU83eDQomHdrch826");
@@ -21,12 +21,12 @@ pub mod escrow {
         offered_amount: u64,
         wanted_amount: u64,
     ) -> Result<()> {
-        send_offered_tokens_to_vault(&context, offered_amount)?;
-        save_offer(context, id, wanted_amount)
+        handlers::make_offer::send_offered_tokens_to_vault(&context, offered_amount)?;
+        handlers::make_offer::save_offer(context, id, wanted_amount)
     }
 
     pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
-        send_wanted_tokens_to_maker(&context)?;
-        withdraw_and_close_vault(context)
+        handlers::take_offer::send_wanted_tokens_to_maker(&context)?;
+        handlers::take_offer::withdraw_and_close_vault(context)
     }
 }
