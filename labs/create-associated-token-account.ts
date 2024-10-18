935dd937673278f5847dcd8cfae68f80a5c9b28b
Mike MacCana <mike.maccana@gmail.com>
2024-10-18 16:37:48 -0400

    Remove duplicate script and update create-associated-token-account.ts

--- labs/create-associated-token-account.ts
+++ labs/create-associated-token-account.ts
@@ -1,4 +1,7 @@
-import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import {
+  getOrCreateAssociatedTokenAccount,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
 import "dotenv/config";
 import {
   getExplorerLink,
@@ -14,16 +17,24 @@ console.log(
 );
 
 // Subtitute in your token mint account from create-token-mint.ts
-const tokenMintAccount = new PublicKey("YOUR_TOKEN_MINT_ADDRESS_HERE");
+const tokenMintAccount = new PublicKey(
+  "9vrkZjCuukcfC9jZ3UGtf3GjUtWet6ZbkxDJqtf9WyvJ"
+);
 
 // Substitute in your classmate's wallet address here
-const recipient = new PublicKey("Hxsgo2tPiu6967VUaEquk232riDKkaqK89wBvdSCgjH7");
+const recipient = new PublicKey("5JhJhCj5yXhnZxtPRUvAnzGBfQNJhY2js4HoLVXzBTmG");
+
+console.log(`⏳ Creating associated token account...`);
 
 const tokenAccount = await getOrCreateAssociatedTokenAccount(
   connection,
   user,
   tokenMintAccount,
-  recipient
+  recipient,
+  false,
+  "confirmed",
+  {},
+  TOKEN_2022_PROGRAM_ID
 );
 
 console.log(`Token Account: ${tokenAccount.address.toBase58()}`);
@@ -34,4 +45,4 @@ const link = getExplorerLink(
   "devnet"
 );
 
-console.log(`✅ Success! Created token Account: ${link}`);
+console.log(`✅ Success! Created associated token Account: ${link}`);
